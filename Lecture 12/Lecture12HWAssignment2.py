696c2a6a9616ff755a38b4e421a409429a55e9c4
AtharvaJoshi21 <joshi.atharva92@hotmail.com>
2019-02-23 21:37:04 +0530

    Lecture 12 Homework assignment 2 updated: Added flag of already checked

--- Lecture 12/Lecture12HWAssignment2.py
+++ Lecture 12/Lecture12HWAssignment2.py
@@ -5,12 +5,19 @@
 def BubbleSort(inputList):
     listCount = len(inputList)
     for x in range(listCount - 1):
-        # alreadySorted = True
+        alreadySorted = True
+        print('Iteration', x)
         for y in range(x+1, listCount - 1):
             if inputList[x] > inputList[y]:
                 temp = inputList[x]
                 inputList[x] = inputList[y]
                 inputList[y] = temp
+                alreadySorted = False
+            else:
+                alreadySorted = True
+        if alreadySorted:
+            continue
+
     return inputList
 
 def main():
